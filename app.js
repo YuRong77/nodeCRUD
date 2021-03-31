923deaa347bd300c9c22efe317716f8fdf831d1e
Yu Rong <gcobc50335@gmail.com>
2021-03-31 20:21:35 +0800

    設定CORS

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const app = express();
 
-const bodyParser = require('body-parser'); //讀取 req.body 用
-app.use(bodyParser.urlencoded({extended: false}));
+const bodyParser = require("body-parser"); //讀取 req.body    npm install body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const user = require('./routes/user')
-app.use('/user',user)
+const cors = require("cors"); // npm install cors
+const corsOptions = {
+  origin: ["https://yurong77.github.io/blogTest/", "http://127.0.0.1:5500"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+const user = require("./routes/user");
+app.use("/user", user);
 
 app.get("/", (req, res) => {
-  res.send('hello')
+  res.send("hello");
 });
 
-
-const fireData = require('./connections/firebase_admin')
+// const fireData = require('./connections/firebase_admin')
 // fireData
 //   .ref("test")
 //   .set({ title2: "hello" })
@@ -24,12 +31,12 @@ const fireData = require('./connections/firebase_admin')
 //   });
 
 // fireData.ref("test").once("value", (snapshot) => {
-//   const list = snapshot.val() 
+//   const list = snapshot.val()
 //   console.log(list);
 // });
 
 // fireData.ref("test").once("value").then( snapshot => {
-//   const list = snapshot.val() 
+//   const list = snapshot.val()
 // });
 
 const port = process.env.PORT || 8800;
