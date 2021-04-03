952e46f88fd3144a7fbc77faae27df6531e0b1d6
Yu Rong <gcobc50335@gmail.com>
2021-04-03 10:47:01 +0800

    add 錯誤判斷

--- app.js
+++ app.js
@@ -5,39 +5,20 @@ const bodyParser = require("body-parser"); //讀取 req.body    npm install body
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const cors = require("cors"); // npm install cors
+const cors = require("cors"); // 處理CORS npm install cors
 const corsOptions = {
   origin: ["https://yurong77.github.io/blogTest/", "http://127.0.0.1:5500"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   allowedHeaders: ["Content-Type", "Authorization"],
 };
-
 app.use(cors(corsOptions));
+
 const user = require("./routes/user");
 app.use("/user", user);
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
-
-// const fireData = require('./connections/firebase_admin')
-// fireData
-//   .ref("test")
-//   .set({ title2: "hello" })
-//   .then(() => {
-//     fireData.ref("test").once("value", (snapshot) => {
-//       console.log(snapshot.val());
-//     });
-//   });
-
-// fireData.ref("test").once("value", (snapshot) => {
-//   const list = snapshot.val()
-//   console.log(list);
-// });
-
-// fireData.ref("test").once("value").then( snapshot => {
-//   const list = snapshot.val()
-// });
 
 const port = process.env.PORT || 8800;
 app.listen(port, () => {
--- routes/user.js
+++ routes/user.js
@@ -6,17 +6,17 @@ const user = fireData.ref("user"); //通常同一支檔案 都是改同一個資
 router.get("/getList", (req, res) => {
   user.once("value").then((snapshot) => {
     const list = snapshot.val();
-    const List = []
-    Object.keys(list).forEach(key=>{
+    const List = [];
+    Object.keys(list).forEach((key) => {
       List.push({
         id: key,
-        content:list[key].content
-      })
-    })
-    res.json({
-      success: true,
+        content: list[key].content,
+      });
+    });
+    res.status(200).json({
+      status: 200,
       Result: List,
-      message: '取得清單'
+      message: "取得清單",
     });
   });
 });
@@ -24,31 +24,45 @@ router.get("/getList", (req, res) => {
 router.post("/addList", (req, res) => {
   console.log(req.body); // 前端傳來的key值 都會在body中
   const content = req.body.content;
-  user
-    .push()
-    .set({ content: content })
-    .then(() => {
-      // push產生亂數key並新增一筆  set則全部覆蓋  官方使用.push().set()  如果只用.push({...}) 會無法抓取key
-      res.json({
-        success: true,
-        content: content,
-        message: "新增成功",
+  if (content) {
+    user
+      .push()
+      .set({ content: content })
+      .then(() => {
+        // push產生亂數key並新增一筆  set則全部覆蓋  官方使用.push().set()  如果只用.push({...}) 會無法抓取key
+        res.status(200).json({
+          status: 200,
+          content: content,
+          message: "新增成功",
+        });
       });
+  } else {
+    res.status(400).json({
+      status: 400,
+      message: "資料不正確",
     });
+  }
 });
 
 router.post("/deleteList", (req, res) => {
   const _id = req.body.id;
-  user
-    .child(_id)
-    .remove()
-    .then(() => {
-      res.json({
-        success: true,
-        id: _id,
-        message: "刪除成功",
+  if (_id) {
+    user
+      .child(_id)
+      .remove()
+      .then(() => {
+        res.status(200).json({
+          status: 200,
+          id: _id,
+          message: "刪除成功",
+        });
       });
+  } else {
+    res.status(400).json({
+      status: 400,
+      message: "資料不正確",
     });
+  }
 });
 
 module.exports = router;
